eba80e6f56656e4ddbf55feb64fc564b18c733b2
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-03-07 14:54:20 -0300

    Building Products Controller

---
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if(product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if(productVO == null) return BadRequest();
+            var products = await _productRepository.Create(productVO);
+            return Ok(products);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if (productVO == null) return BadRequest();
+            var products = await _productRepository.Update(productVO);
+            return Ok(products);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var status = _productRepository.Delete(id);
+            if(status == null) return BadRequest();
+            return Ok(true);
+
+        }
+    }
+}
