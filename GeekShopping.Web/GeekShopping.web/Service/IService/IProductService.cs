669eedd8ff0d62eef3f28cc83528275de557f3b1
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-03-10 12:19:21 -0300

    building product service

---
+++ GeekShopping.Web/GeekShopping.web/Service/IService/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using GeekShopping.web.Models;
+
+namespace GeekShopping.web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+
+        Task<ProductModel> FindProductById(long id);
+
+        Task<ProductModel> CreateProduct(ProductModel model);
+
+        Task<ProductModel> UpdateProduct(ProductModel model);
+
+        Task<bool> DeleteProductById(long id);
+
+    }
+}
---
+++ GeekShopping.Web/GeekShopping.web/Service/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using GeekShopping.web.Models;
+using GeekShopping.web.Service.IService;
+using GeekShopping.web.Utils;
+using System.Net.Http.Headers;
+using System.Reflection;
+using static System.Net.WebRequestMethods;
+
+namespace GeekShopping.web.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath =$"api/v1/product";
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _httpClient.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _httpClient.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            var response = await _httpClient.PostAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode) return await response.ReadContentAs<ProductModel>();
+            else throw new Exception($"Deu ruim no post na hora de criar {response.ReasonPhrase}");
+            
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            var response = await _httpClient.PutAsJsonAsync(BasePath, model);
+            if (response.IsSuccessStatusCode) return await response.ReadContentAs<ProductModel>();
+            else throw new Exception($"Deu ruim no put na hora de criar {response.ReasonPhrase}");
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _httpClient.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode) return await response.ReadContentAs<bool>();
+            else throw new Exception($"Deu ruim no Delete na hora de criar {response.ReasonPhrase}");
+        }
+    }
+}
