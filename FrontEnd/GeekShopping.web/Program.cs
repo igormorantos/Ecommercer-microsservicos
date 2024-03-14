694e57324bf5e3da8cfea80afef1254edcdc1706
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-03-14 09:00:27 -0300

    entering the packages/urls/authentication in web microsservice

--- FrontEnd/GeekShopping.web/Program.cs
+++ FrontEnd/GeekShopping.web/Program.cs
@@ -1,13 +1,35 @@
 using GeekShopping.web.Service;
 using GeekShopping.web.Service.IService;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductApi"])
+);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpClient<IProductService, ProductService>(c =>
-    c.BaseAddress = new Uri(builder.Configuration["ServiveUrls:ProductApi"])
-);
+builder.Services.AddAuthentication(options => 
+{ 
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClientId = "geek_shopping";
+        options.ClientSecret = "my_super_secret";
+        options.ResponseType = "code";
+        options.ClaimActions.MapJsonKey("role","role","role");
+        options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+        options.Scope.Add("geek_shopping");
+        options.SaveTokens = true;
+    });
 
 
 var app = builder.Build();
@@ -17,9 +39,14 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
