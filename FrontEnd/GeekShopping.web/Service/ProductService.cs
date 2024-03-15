c0d4598f534ba985379aea48164b700c04f3d9ac
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-03-14 21:02:05 -0300

    adding authentication to the service

--- FrontEnd/GeekShopping.web/Service/ProductService.cs
+++ FrontEnd/GeekShopping.web/Service/ProductService.cs
@@ -17,35 +17,40 @@ namespace GeekShopping.web.Service
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
 
-        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        public async Task<IEnumerable<ProductModel>> FindAllProducts(string token)
         {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.GetAsync(BasePath);
             return await response.ReadContentAs<List<ProductModel>>();
         }
 
-        public async Task<ProductModel> FindProductById(long id)
+        public async Task<ProductModel> FindProductById(long id, string token)
         {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.GetAsync($"{BasePath}/{id}");
             return await response.ReadContentAs<ProductModel>();
         }
 
-        public async Task<ProductModel> CreateProduct(ProductModel model)
+        public async Task<ProductModel> CreateProduct(ProductModel model, string token)
         {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.PostAsJson(BasePath, model);
             if (response.IsSuccessStatusCode) return await response.ReadContentAs<ProductModel>();
             else throw new Exception($"Deu ruim no post na hora de criar {response.ReasonPhrase}");
             
         }
 
-        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        public async Task<ProductModel> UpdateProduct(ProductModel model, string token)
         {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.PutAsJsonAsync(BasePath, model);
             if (response.IsSuccessStatusCode) return await response.ReadContentAs<ProductModel>();
             else throw new Exception($"Deu ruim no put na hora de criar {response.ReasonPhrase}");
         }
 
-        public async Task<bool> DeleteProductById(long id)
+        public async Task<bool> DeleteProductById(long id, string token)
         {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var response = await _httpClient.DeleteAsync($"{BasePath}/{id}");
             if (response.IsSuccessStatusCode) return await response.ReadContentAs<bool>();
             else throw new Exception($"Deu ruim no Delete na hora de criar {response.ReasonPhrase}");
