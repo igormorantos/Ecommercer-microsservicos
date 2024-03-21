2ae20756e3b34a7fa15814f34540a5ef0017df7b
igor <igor@DESKTOP-H1HTBV1>
2024-03-21 20:06:52 -0300

    building Coupon Controller

---
+++ Services/GeekShopping.CouponAPI/Controller/CouponController.cs
@@ -0,0 +1,29 @@
+﻿using GeekShopping.CouponAPI.Data.ValueObjects;
+using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CouponAPI.Controller
+{
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CouponController : ControllerBase
+    {
+        private ICouponRepository _couponRepository;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository ?? throw new ArgumentNullException(nameof(couponRepository)); ;
+        }
+
+        [HttpGet("{couponCode}")]
+        [Authorize]
+        public async Task<ActionResult<CouponVO>> GetCouponByCouponCode(string couponCode)
+        {
+            var coupon = await _couponRepository.GetCouponByCouponCode(couponCode);
+            if (coupon.Id <= 0) return NotFound();
+            return Ok(coupon);
+        }
+    }
+}
