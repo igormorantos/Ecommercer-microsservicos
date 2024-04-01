841f087acfdbdbce51c867081120f9062fbce3d4
igor <igor@DESKTOP-H1HTBV1>
2024-03-31 22:44:26 -0300

    Inserting the messaging service with rabbitMQ into PaymentAPI

---
+++ GeekShopping.PaymentAPI/RabbitMQSender/IRabbitMQMessageSender.cs
@@ -0,0 +1,10 @@
+﻿using GeekShopping.MessageBus;
+using System.Buffers.Text;
+
+namespace GeekShopping.PaymentAPI.RabbitMQSender
+{
+    public interface IRabbitMQMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage);
+    }
+}
